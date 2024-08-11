321dbf79a2872453b0bf93e4e7dadc68fac07a58
Ismael <ismaelguedessilva@gmail.com>
2024-08-11 11:48:40 -0300

    add get terms function

--- wp-content/themes/odonto/page-servicos.php
+++ wp-content/themes/odonto/page-servicos.php
@@ -1,7 +1,23 @@
 <?php
 $estiloPagina = 'servicos.css';
 require_once 'header.php';
+?>
 
+<form action="#" class="container-odonto formulario-pesquisa-servicos">
+    <h2>Conheça nossos destinos</h2>
+    <select name="servicos" id="servicos">
+        <option value="">--Selecione--</option>
+        <?php
+            $servicos = get_terms(array('taxonomy' => 'servicos'));
+            foreach($servicos as $servico):?>
+                <option value="<?= $servico->name ?>"><?= $servico->name ?></option>
+            <?php endforeach;
+        ?>
+    </select>
+    <input type="submit" value="Pesquisar">
+</form>
+
+<?php
 $args = array('post_type' => 'servicos' );
 $query = new WP_Query($args);
 if ($query->have_posts()):
