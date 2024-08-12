31aa821b6ed6b495cbfd6ac332f136bd0d4303b2
Ismael <ismaelguedessilva@gmail.com>
2024-08-12 11:47:35 -0300

    add cdn bootstrap

--- wp-content/themes/odonto/header.php
+++ wp-content/themes/odonto/header.php
@@ -8,6 +8,9 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php bloginfo('name'); ?></title>
     <?php wp_head(); ?>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
     <link rel="stylesheet" href="<?= get_template_directory_uri() . '/css/normalize.css' ?>">
     <link rel="stylesheet" href="<?= get_template_directory_uri() . '/css/bootstrap.css' ?>">
     <link rel="stylesheet" href="<?= get_template_directory_uri() . '/css/header.css' ?>">
@@ -20,7 +23,7 @@
 <body <?php body_class(); ?>>
     <header class="site-header informations">
         <div class="container-odonto">
-            <div style="gap: 100px;">
+            <div style="gap: 50px;">
                 <?php
 
                 $args = array(
@@ -38,13 +41,14 @@
 
                     while ($query->have_posts()): $query->the_post();
                     ?>
-                        <div class="itens-information" style="color: white">
-                         <img class="icon-content" width="20" height="20" src="<?php the_post_thumbnail_url()?>">
-
-                        <?php
+                        <div class="itens-information" style="color: white; gap: 5px;">
+                            <img class="icon-content" width="20" height="20" src="<?php the_post_thumbnail_url()?>">
+                      <div class="content-itens mt-3">
+                      <?php
                         the_content();
                         ?>
-                        </div>
+                       </div>
+                    </div>
                 <?php
                     endwhile;
                 endif;
@@ -63,7 +67,7 @@
             <?php
             the_custom_logo();
             ?>
-            <nav>
+            <nav class="menu-main" style="margin-left: -35%;">
                 <?php
                 wp_nav_menu(
                     array(
@@ -73,5 +77,8 @@
                 );
                 ?>
             </nav>
+            <div class="button-appointment">
+            <button type="button" class="btn btn-primary btn-appointment">Marcar Consulta</button>
+            </div>
         </div>
     </header>
