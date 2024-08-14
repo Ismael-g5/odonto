9f8240142657648390a03bf4e691a0249334257b
Ismael <ismaelguedessilva@gmail.com>
2024-08-14 17:07:30 -0300

    add others services

--- wp-content/themes/odonto/front-page.php
+++ wp-content/themes/odonto/front-page.php
@@ -11,23 +11,22 @@ $args = array(
 $query = new WP_Query($args);
 if ($query->have_posts()):
     while ($query->have_posts()): $query->the_post();
-?>
-        <main>
-            <div class="imagem-banner">
-                <img class="img-banner-home" style="height: 80vh; width: 100%;" src="<?php the_post_thumbnail_url(); ?>">
-            </div>
-            <div class="texto-banner-dinamico">
-                <span id="texto-banner"></span>
-            </div>
-        </main>
-<?php
+    ?>
+    <main>
+        <div class="imagem-banner">
+            <img class="img-banner-home" style="height: 80vh; width: 100%;" src="<?php the_post_thumbnail_url(); ?>">
+        </div>
+        <div class="texto-banner-dinamico">
+            <span id="texto-banner"></span>
+        </div>
+    </main>
+    <?php
     endwhile;
 endif;
 ?>
 <div class="container">
     <div class="row services">
         <div class="col-md-6">
-
             <?php
             // Recupera o valor do metabox para o tipo de post 'sobre_nos'
             $argsAB = array(
@@ -56,47 +55,50 @@ endif;
                     </div>
                     <hr class="line-separation mt-5">
 
-                    <div class="icon_1">
-
+                    <div class="icons">
                         <?php
-                        // Recupera o grupo de campos 'icones_texto'
                         $icones_texto = get_field('icones_texto');
-
-                        if ($icones_texto) {
-                            // Suponha que $icones_texto seja um array com vários grupos
-                            foreach ($icones_texto as $item) {
-                                $icone_url = $item['icone']['url']; // Subcampo 'icone', acessando a URL da imagem
-                                $texto = $item['texto']; // Subcampo 'texto'
-
-                                // Exibir os dados
-                        ?>
-                                <div class="icone-texto-item">
-                                    <?php if (!empty($icone_url)): ?>
-                                        <img src="<?php echo esc_url($icone_url); ?>" alt="Ícone">
-                                    <?php endif; ?>
-                                    <p><?php echo esc_html($texto); ?></p>
+                        if ($icones_texto): ?>
+                            <div id="icones_texto_itens" class="row icones_itens">
+                                <div class="col-md-4 icon-text-1">
+                                    <div class="image-icon-1 d-flex align-items-center justify-content-center">   
+                                        <img src="<?php echo esc_url($icones_texto['icone_1']); ?>" alt="Icone 1" />
+                                    </div>    
+                                    <div class="content texto-1 text-center">
+                                        <b><?php echo esc_html($icones_texto['texto_1']); ?></b>
+                                    </div>
                                 </div>
-                        <?php
-                            }
-                        } else {
-                            echo 'Nenhum ícone e texto encontrado.';
-                        }
-                        ?>
 
-
-
+                                <div class="col-md-4 icon-text-2">
+                                    <div class="image-icon-2 d-flex align-items-center justify-content-center">    
+                                        <img src="<?php echo esc_url($icones_texto['icone_2']); ?>" alt="Icone 2" />
+                                    </div>    
+                                    <div class="content texto-2 text-center">
+                                        <b><?php echo esc_html($icones_texto['texto_2']); ?></b>
+                                    </div>
+                                </div>
+                                <div class="col-md-4 icon-text-3">
+                                    <div class="image-icon-3 d-flex align-items-center justify-content-center">    
+                                        <img src="<?php echo esc_url($icones_texto['icone_3']); ?>" alt="Icone 3" />
+                                    </div>    
+                                    <div class="content texto-3 text-center">
+                                        <b><?php echo esc_html($icones_texto['texto_3']); ?></b>
+                                    </div>
+                                </div>
+                            </div>
+                        <?php endif; ?>
                     </div>
             <?php
                 endwhile;
             endif;
             ?>
-
         </div>
         <div class="col-md-6 text-center">
             <img width="400" height="500" src="<?php the_post_thumbnail_url(); ?>">
         </div>
     </div>
 </div>
+
 
 <?php
 require_once 'footer.php';
