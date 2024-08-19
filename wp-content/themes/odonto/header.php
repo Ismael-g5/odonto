a9eec01a94d6be1c91007eb2c08b73d9c7e77ac9
Ismael <ismaelguedessilva@gmail.com>
2024-08-19 09:34:09 -0300

    fix header

--- wp-content/themes/odonto/header.php
+++ wp-content/themes/odonto/header.php
@@ -8,11 +8,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php bloginfo('name'); ?></title>
     <?php wp_head(); ?>
-    
+
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="<?= get_template_directory_uri() . '/assets/css/global.css' ?>">
     <link rel="stylesheet" href="<?= get_template_directory_uri() . '/assets/css/normalize.css' ?>">
@@ -51,7 +51,10 @@
             <?php $urlTemplate = esc_url(get_template_directory_uri()); ?>
             <div class="icon-socials" style="gap: 20px;">
                 <div class="icon-whatsapp">
-                    <img src="<?php echo $urlTemplate; ?>/assets/images/icon-whz.png">
+                    <a href="#">
+
+                        <img src="<?php echo $urlTemplate; ?>/assets/images/icon-whz.png">
+                    </a>
                 </div>
                 <div class="icon-instagram">
                     <a href="#">
@@ -118,7 +121,7 @@
         </div>
     </div>
 
-   
+
 
     <?php wp_footer(); ?>
 </body>
